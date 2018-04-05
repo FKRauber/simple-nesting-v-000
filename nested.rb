36d28d39cd9bb1fc1080231a59287fd8c9e6ffba
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-05 21:16:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	dennis_hash = programmer_hash[:dennis_ritchie][:languages]
+	programmer_hash[:dennis_ritchie][:languages]
 end
 
 
